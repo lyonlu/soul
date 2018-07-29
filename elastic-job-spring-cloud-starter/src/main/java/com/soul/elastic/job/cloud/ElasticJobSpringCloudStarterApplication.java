8ca17632eb7dcb8e49c5fe61d4db71ff4061c631
sumy <wustlledu@sina.com>
2018-07-29 22:27:25 +0800

    elastic job 学习
    Signed-off-by: sumy <wustlledu@sina.com>

---
+++ elastic-job-spring-cloud-starter/src/main/java/com/soul/elastic/job/cloud/ElasticJobSpringCloudStarterApplication.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  Copyright (c) 2017-2018 Soul, sumy  All rights reserved.
+ *
+ *  Permission is hereby granted, free  of charge, to any person obtaining
+ *  a  copy  of this  software  and  associated  documentation files  (the
+ *  "Software"), to  deal in  the Software without  restriction, including
+ *   without limitation  the rights to  use, copy, modify,  merge, publish,
+ *   distribute,  sublicense, and/or sell  copies of  the Software,  and to
+ *   permit persons to whom the Software  is furnished to do so, subject to
+ *   the following conditions:
+ *
+ *   The  above  copyright  notice  and  this permission  notice  shall  be
+ *   included in all copies or substantial portions of the Software.
+ *
+ */
+
+package com.soul.elastic.job.cloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
+public class ElasticJobSpringCloudStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobSpringCloudStarterApplication.class, args);
+    }
+}
